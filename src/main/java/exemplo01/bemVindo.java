2ac7678e67054fb77ce2af78305cde8162fa1e40
Liziane Lima <lizifenix@gmail.com>
2022-06-15 23:00:07 -0300

    Segundo Commit

--- src/main/java/exemplo01/bemVindo.java
+++ src/main/java/exemplo01/bemVindo.java
@@ -3,5 +3,7 @@ package exemplo01;
 public class bemVindo {
     public static void main(String[] args) {
         System.out.println("Oi");
+        System.out.println("Mundo");
+        System.out.println("Teste");
     }
 }
